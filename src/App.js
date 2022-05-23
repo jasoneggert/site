36c21c4b08c426e2fd1c70fdce36969f5db48aad
Jason Eggert <jasoneggert@Jasons-MacBook-Pro.local>
2022-05-23 18:43:23 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from 'three'
+import { useState, useRef, Suspense, useMemo } from 'react'
+import { Canvas, useThree, useFrame, useLoader } from '@react-three/fiber'
+import { Reflector, CameraShake, OrbitControls, useTexture } from '@react-three/drei'
+import { KernelSize } from 'postprocessing'
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader'
+import styled from 'styled-components'
 
-function App() {
+const Screen = styled.div`
+  height: 100vh;
+  width: 100vw;
+`
+
+
+const Name = styled.div`
+  position: fixed;
+  top: 306px;
+  left: 45px;
+  color: white;
+  z-index: 1200;
+  font-size: 36px;
+`
+const Subtitle = styled.div`
+  position: fixed;
+  top: 351px;
+  left: 45px;
+  color: white;
+  z-index: 1200;
+`
+
+const LinkedInLink = styled.a`
+position: fixed;
+  color: cyan;
+  top: 378px;
+  left: 45px;
+  font-size: 18px;
+  z-index: 1200;
+
+`
+
+function Triangle({ color, ...props }) {
+  const ref = useRef()
+  const [r] = useState(() => Math.random() * 10000)
+  useFrame((_) => (ref.current.position.y = -1.75 + Math.sin(_.clock.elapsedTime + r) / 10))
+  const { paths: [path] } = useLoader(SVGLoader, '/triangle.svg') // prettier-ignore
+  const geom = useMemo(() => SVGLoader.pointsToStroke(path.subPaths[0].getPoints(), path.userData.style), [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <group ref={ref}>
+      <mesh geometry={geom} {...props}>
+        <meshBasicMaterial color={color} toneMapped={false} />
+      </mesh>
+    </group>
+  )
 }
 
-export default App;
+function Rig({ children }) {
+  const ref = useRef()
+  const vec = new THREE.Vector3()
+  const { camera, mouse } = useThree()
+  useFrame(() => {
+    camera.position.lerp(vec.set(mouse.x * 2, 0, 3.5), 0.05)
+    ref.current.position.lerp(vec.set(mouse.x * 1, mouse.y * 0.1, 0), 0.1)
+    ref.current.rotation.y = THREE.MathUtils.lerp(ref.current.rotation.y, (-mouse.x * Math.PI) / 20, 0.1)
+  })
+  return <group ref={ref}>{children}</group>
+}
+
+function Ground(props) {
+  const [floor, normal] = useTexture(['/SurfaceImperfections003_1K_var1.jpg', '/SurfaceImperfections003_1K_Normal.jpg'])
+  return (
+    <Reflector resolution={1024} args={[8, 8]} {...props}>
+      {(Material, props) => <Material color="#f0f0f0" metalness={0} roughnessMap={floor} normalMap={normal} normalScale={[2, 2]} {...props} />}
+    </Reflector>
+  )
+}
+
+export default function App() {
+  console.log('hello')
+  return (
+    <Screen>
+      <Name>Jason Eggert</Name>
+      <Subtitle>Product  / UX / Engineer</Subtitle>
+      <LinkedInLink href="https://www.linkedin.com/in/jasoneggert/">LinkedIn</LinkedInLink>
+    <Canvas dpr={[1, 1.5]} camera={{ position: [0, 0, 15] }}>
+      <color attach="background" args={['black']} />
+      <ambientLight />
+      <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
+      <Suspense fallback={null}>
+        <Rig>
+          <Triangle color="#00FFFF" scale={0.009} rotation={[0, 0, 0]} position={[-1, 1, 0]} />
+          <Triangle color="#ff2060" scale={0.009} rotation={[0, 0, 0]} position={[-1, 1, 0]} />
+
+          <Ground mirror={1} blur={[500, 100]} mixBlur={12} mixStrength={1.5} rotation={[-Math.PI / 2, 0, Math.PI / 2]} position-y={-0.8} />
+        </Rig>
+        <EffectComposer multisampling={8}>
+          <Bloom kernelSize={3} luminanceThreshold={0} luminanceSmoothing={0.4} intensity={0.6} />
+          <Bloom kernelSize={KernelSize.HUGE} luminanceThreshold={0} luminanceSmoothing={0} intensity={0.5} />
+        </EffectComposer>
+      </Suspense>
+      {/* <CameraShake yawFrequency={0.2} pitchFrequency={0.2} rollFrequency={0.2} /> */}
+    </Canvas>
+    </Screen>
+  )
+}
+
